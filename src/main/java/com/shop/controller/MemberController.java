8c6caa3059b38073183888e6e5e695a7f8d1ff4e
jiminhwang94 <126554189+jiminhwang94@users.noreply.github.com>
2023-08-10 22:21:13 +0900

    8/10 10:21 commit !!!

--- src/main/java/com/shop/controller/MemberController.java
+++ src/main/java/com/shop/controller/MemberController.java
@@ -7,9 +7,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @RequestMapping("/members")
 @Controller
@@ -18,6 +21,25 @@ public class MemberController {
 
     private final MemberService memberService;
     private final PasswordEncoder passwordEncoder;
+
+
+//    @PostMapping(value = "/new")
+//    public String newMember(@Valid MemberFormDto memberFormDto, BindingResult bindingResult, Model model){
+//
+//        if(bindingResult.hasErrors()){
+//            return "member/memberForm";
+//        }
+//
+//        try {
+//            Member member = Member.createMember(memberFormDto, passwordEncoder);
+//            memberService.saveMember(member);
+//        } catch (IllegalStateException e){
+//            model.addAttribute("errorMessage", e.getMessage());
+//            return "member/memberForm";
+//        }
+//
+//        return "redirect:/";
+//    }
 
     @GetMapping(value = "/new")
     public String memberForm(Model model){
