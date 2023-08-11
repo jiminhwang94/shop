004f4e7282794e3a6bfbd761e5ea16f4a56dc062
ghkdwlals0 <ghkdwlals0@naver.com>
2023-08-11 11:27:36 +0900

    8/11 CSS 오류 해결. 이유는 <div th:replace="~{fragments/footer::footer}"></div> 여기서 인식을 못했던 오류. 코드에 문제점을 못찾아서 강사님과 고민후 인식 오류로 다시 코드를 작성. 그 후 오류 해결

--- src/main/java/com/shop/controller/MemberController.java
+++ src/main/java/com/shop/controller/MemberController.java
@@ -23,23 +23,6 @@ public class MemberController {
     private final PasswordEncoder passwordEncoder;
 
 
-//    @PostMapping(value = "/new")
-//    public String newMember(@Valid MemberFormDto memberFormDto, BindingResult bindingResult, Model model){
-//
-//        if(bindingResult.hasErrors()){
-//            return "member/memberForm";
-//        }
-//
-//        try {
-//            Member member = Member.createMember(memberFormDto, passwordEncoder);
-//            memberService.saveMember(member);
-//        } catch (IllegalStateException e){
-//            model.addAttribute("errorMessage", e.getMessage());
-//            return "member/memberForm";
-//        }
-//
-//        return "redirect:/";
-//    }
 
     @GetMapping(value = "/new")
     public String memberForm(Model model){
@@ -47,12 +30,26 @@ public class MemberController {
         return "member/memberForm";
     }
 
+    //@Valid: 이 어노테이션은 MemberFormDto 객체가 유효성 검사를 받아야 함
+    //MemberFormDto: 폼에서 제출된 데이터
+    //BindingResult: 유효성 검사 중 발생한 오류와 관련된 정보를 담는 객체
+    //Model: 뷰와 컨트롤러 사이에서 데이터를 전달하기 위한 객체
     @PostMapping(value = "/new")
-    public String memberForm(MemberFormDto memberFormDto){
+    public String newMember(@Valid MemberFormDto memberFormDto, BindingResult bindingResult, Model model){
 
-        Member member = Member.createMember(memberFormDto, passwordEncoder);
-        memberService.saveMember(member);
+        if(bindingResult.hasErrors()){
+            return "member/memberForm";
+        }
+
+        try {
+            Member member = Member.createMember(memberFormDto, passwordEncoder);
+            memberService.saveMember(member);
+        } catch (IllegalStateException e){
+            model.addAttribute("errorMessage", e.getMessage());
+            return "member/memberForm";
+        }
 
         return "redirect:/";
     }
+
 }
